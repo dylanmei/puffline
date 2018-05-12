c5d62a3328dd25a6fd229753a0c9fcc893941c85
Dylan Meissner <dylanmei@gmail.com>
2018-05-12 08:38:57 -0700

    Add k8s context and terraform workspace

--- main.go
+++ main.go
@@ -13,7 +13,9 @@ import (
 )
 
 const PathStyle = "250+h:238"
-const GitStyle = "250:93"
+const GitStyle = "250:22"
+const KubernetesStyle = "250:21"
+const TerraformStyle = "250:93"
 const ExitCodeStyle = "250:124"
 
 func main() {
@@ -31,12 +33,34 @@ func main() {
 	fmt.Fprintln(os.Stdout)
 	path(os.Stdout, pwd)
 	git(os.Stdout, pwd)
+	kubernetes(os.Stdout)
+	terraform(os.Stdout, pwd)
 
 	fmt.Fprintln(os.Stdout)
 	exitCode(os.Stdout, flag.Args())
 	prompt(os.Stdout)
 
 	//colors(os.Stdout)
+}
+
+func path(w io.Writer, pwd string) {
+	path := pwd
+	if user, err := user.Current(); err == nil {
+		if strings.HasPrefix(path, user.HomeDir) {
+			path = "~" + path[len(user.HomeDir):]
+		}
+	}
+
+	pathSegments := strings.Split(path, "/")
+	viewSegments := []string{}
+	for i := 0; i < len(pathSegments)-1; i++ {
+		pathSegment := pathSegments[i]
+		viewSegments = append(viewSegments, pathSegment[0:1])
+	}
+
+	viewSegments = append(viewSegments, pathSegments[len(pathSegments)-1])
+	fmt.Fprint(w, ansi.Color(fmt.Sprintf(" %s ", strings.Join(viewSegments, "/")), PathStyle))
+	return
 }
 
 func git(w io.Writer, pwd string) {
@@ -77,24 +101,29 @@ func git(w io.Writer, pwd string) {
 	}
 }
 
-func path(w io.Writer, pwd string) {
-	path := pwd
-	if user, err := user.Current(); err == nil {
-		if strings.HasPrefix(path, user.HomeDir) {
-			path = "~" + path[len(user.HomeDir):]
-		}
+func kubernetes(w io.Writer) {
+	cmd := exec.Command("kubectl", "config", "current-context")
+	stdout, _ := cmd.Output()
+
+	if len(stdout) == 0 {
+		return
 	}
 
-	pathSegments := strings.Split(path, "/")
-	viewSegments := []string{}
-	for i := 0; i < len(pathSegments)-1; i++ {
-		pathSegment := pathSegments[i]
-		viewSegments = append(viewSegments, pathSegment[0:1])
+	context := strings.TrimRight(string(stdout), "\n")
+	fmt.Fprintf(w, ansi.Color(fmt.Sprintf(" %s ", context), KubernetesStyle))
+}
+
+func terraform(w io.Writer, pwd string) {
+	cmd := exec.Command("terraform", "workspace", "show")
+	stdout, _ := cmd.Output()
+
+	if len(stdout) == 0 {
+		return
 	}
 
-	viewSegments = append(viewSegments, pathSegments[len(pathSegments)-1])
-	fmt.Fprint(w, ansi.Color(fmt.Sprintf(" %s ", strings.Join(viewSegments, "/")), PathStyle))
-	return
+	if workspace := strings.TrimRight(string(stdout), "\n"); workspace != "default" {
+		fmt.Fprintf(w, ansi.Color(fmt.Sprintf(" %s ", workspace), TerraformStyle))
+	}
 }
 
 func exitCode(w io.Writer, args []string) {
